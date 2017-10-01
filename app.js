0c621d6e8307a68ed6a36f12d68295c8c2f257c9
Renjith Abby <renjith.abby@gtech.com>
2017-10-01 10:57:02 +0100

    updated

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var cors = require('cors');
 var path = require('path');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -23,6 +24,9 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.set('superSecret', "godslove");
 
+// uncomment after placing your favicon in /public
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -37,9 +41,9 @@ app.use(function(req,res,next){
     next();
 });
 
-/*const staticFiles = express.static(path.join(__dirname, './client/build'))
+const staticFiles = express.static(path.join(__dirname, './client/build'))
 app.use(staticFiles);
-*/
+
 
 
 app.use('/', index);
