fda56575011ee38f1facd0267765c77c31f3ba73
Renjith Abby <renjith.abby@gtech.com>
2017-10-01 11:59:18 +0100

    updated

--- app.js
+++ app.js
@@ -20,12 +20,12 @@ var app = express();
 
 app.use(cors());
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+//app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.set('superSecret', "godslove");
 
 // uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
