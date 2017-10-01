7ebef942350ec96323e70abae82e2066f8836c1b
Renjith Abby <renjith.abby@gtech.com>
2017-10-01 12:05:17 +0100

    updated

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ var app = express();
 
 app.use(cors());
 // view engine setup
-//app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.set('superSecret', "godslove");
 
