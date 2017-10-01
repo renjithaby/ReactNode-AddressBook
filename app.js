5222a01d60dfabb332595253f55f70a846f384d5
Renjith Abby <renjith.abby@gtech.com>
2017-10-01 13:12:34 +0100

    updated

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-//app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 // Make our db accessible to our router
